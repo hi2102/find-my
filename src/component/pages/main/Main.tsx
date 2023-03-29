e6005459bd56bd3d9668c5bad9077b758fd7b725
hi2102 <hi2102@naver.com>
2023-03-29 15:57:03 +0900

    FEAT: Create Main Page

---
+++ src/component/pages/main/Main.tsx
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+import './Main.css';
+import styles from './Main.module.css';
+
+// 메인화면 클릭 움직이는 기능 추가
+
+export default function Main() {
+	const [isSignUpActive, setIsSignUpActive] = useState(false);
+
+	const toggleSignUp = () => {
+		setIsSignUpActive(!isSignUpActive);
+	};
+
+	const handleSubmitSignIn = (e: { preventDefault: () => void }) => {
+		alert('로그인 완료');
+		e.preventDefault();
+	};
+	const handleSubmitSignUp = (e: { preventDefault: () => void }) => {
+		alert('회원 가입 완료');
+		e.preventDefault();
+	};
+
+	return (
+		<div className={`container ${isSignUpActive && 'right-panel-active'}`}>
+			<div className='form-container sign-in-container'>
+				<form onSubmit={handleSubmitSignIn}>
+					<h1 className={styles.title}>Sign in</h1>
+					<input type='email' placeholder='My Email' />
+					<input type='password' placeholder='Enter Password' />
+					<input type='password' placeholder='confirm Password' />
+					<button>Sign In</button>
+				</form>
+			</div>
+			<div className='form-container sign-up-container'>
+				<form onSubmit={handleSubmitSignUp}>
+					<h1 className={styles.title}>Create Account</h1>
+					<input type='text' placeholder='Name' />
+					<input type='email' placeholder='Email' />
+					<input type='password' placeholder='Password' />
+					<input type='password' placeholder='confirm Password' />
+
+					<button>Sign Up</button>
+				</form>
+			</div>
+			<div className='overlay-container'>
+				<div className='overlay'>
+					<div className='overlay-panel overlay-left'>
+						<h2 className={styles.sub_title}>Welcome Back!</h2>
+						<p className={styles.introduce}>
+							To keep connected with us please login with your personal info
+						</p>
+						<button className='ghost' onClick={toggleSignUp}>
+							Sign In
+						</button>
+					</div>
+
+					<div className='overlay-panel overlay-right'>
+						<h2 className={styles.sub_title}>Hello!</h2>
+						<p className={styles.introduce}>
+							Enter your personal details and start journey with us
+						</p>
+						<button className='ghost' onClick={toggleSignUp}>
+							Sign Up
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
